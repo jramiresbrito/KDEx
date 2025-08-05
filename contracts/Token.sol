226f744a252a33296c044d716828a1e08866d145
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2025-08-05 17:06:27 -0300

    Add Symbol and decimals to KDEX Token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.28;
 
 import "hardhat/console.sol";
 
+// Follows the ERC20 standard: https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
 contract Token {
-    string public name = "KDEX";
+    string public name = "Kempaf Decentralized Exchange";
+    string public symbol = "KDEX";
+    uint8 public decimals = 18;
 }
--- test/Token.js
+++ test/Token.js
@@ -4,6 +4,18 @@ describe("Token", () => {
   it("Should set the right name", async () => {
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy();
-    expect(await token.name()).to.equal("KDEX");
+    expect(await token.name()).to.equal("Kempaf Decentralized Exchange");
+  });
+
+  it("Should set the right symbol", async () => {
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+    expect(await token.symbol()).to.equal("KDEX");
+  });
+
+  it("Should set the right decimals", async () => {
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+    expect(await token.decimals()).to.equal(18);
   });
 });
