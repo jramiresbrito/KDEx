ebf7e88a8e62e2fe2985792a86252f31b429cd11
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2025-08-05 17:09:34 -0300

    DRY UP the specs

--- test/Token.js
+++ test/Token.js
@@ -1,21 +1,24 @@
 import { expect } from "chai";
 
 describe("Token", () => {
-  it("Should set the right name", async () => {
+  async function tokenFixture() {
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy();
+    return token;
+  }
+
+  it("Should set the right name", async () => {
+    const token = await tokenFixture();
     expect(await token.name()).to.equal("Kempaf Decentralized Exchange");
   });
 
   it("Should set the right symbol", async () => {
-    const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy();
+    const token = await tokenFixture();
     expect(await token.symbol()).to.equal("KDEX");
   });
 
   it("Should set the right decimals", async () => {
-    const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy();
+    const token = await tokenFixture();
     expect(await token.decimals()).to.equal(18);
   });
 });
