6ec164f44ce260ea55b589d2d64cec38717c36b4
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2025-09-07 15:32:15 -0300

    Add feePercentage

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.28;
 
 contract Exchange {
     address public feeAccount;
+    uint256 public feePercentage;
 
-    constructor(address _feeAccount) {
+    constructor(address _feeAccount, uint256 _feePercentage) {
         feeAccount = _feeAccount;
+        feePercentage = _feePercentage;
     }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 
-async function exchangeFixture(feeAccount) {
+async function exchangeFixture(feeAccount, feePercentage) {
   const Exchange = await ethers.getContractFactory("Exchange");
-  const exchange = await Exchange.deploy(feeAccount);
+  const exchange = await Exchange.deploy(feeAccount, feePercentage);
   return exchange;
 }
 
@@ -11,19 +11,24 @@ const tokens = (n) => {
 }
 
 describe("Exchange", async () => {
-  let exchange, deployer, feeAccount;
+  let exchange, deployer, feeAccount, feePercentage;
 
   beforeEach(async () => {
     let accounts = await ethers.getSigners();
     deployer = accounts[0];
     feeAccount = accounts[1];
+    feePercentage = 1;
 
-    exchange = await exchangeFixture(feeAccount);
+    exchange = await exchangeFixture(feeAccount, feePercentage);
   });
 
   context("Deployment", () => {
     it("Should set the right fee account", async () => {
       expect(await exchange.feeAccount()).to.equal(feeAccount.address);
+    });
+
+    it("Should set the right fee percentage", async () => {
+      expect(await exchange.feePercentage()).to.equal(feePercentage);
     });
   });
 });
