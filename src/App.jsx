ef8782f3116a3e5182d282a35ce7036f3af0fc4a
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2025-09-14 17:19:43 -0300

    Add base FE template

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+    console.log(accounts);
+  }
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {/* Navbar */}
+
+      <main className="exchange grid">
+        <section className="exchange__section--left grid">
+          {/* Markets */}
+
+          {/* Balance */}
+
+          {/* Order */}
+        </section>
+        <section className="exchange__section--right grid">
+          {/* PriceChart */}
+
+          {/* Transactions */}
+
+          {/* Trades */}
+
+          {/* OrderBook */}
+        </section>
+      </main>
+
+      {/* Alert */}
+    </div>
+  );
 }
 
-export default App
+export default App;
